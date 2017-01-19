8648089b5faba97100f854e881e57c9cd04817a8
Aler Denisov <aler.zampillo@gmail.com>
2017-01-19 13:27:10 +0300

    Rentitas utils (RentitasUtility.CollectComponets<PoolType>)

---
+++ Rentitas/Helpers/RentitasUtility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace Rentitas
+{
+    public class ComponentsCollection<TPool>
+            where TPool : IComponent
+    {
+        private HashSet<TPool> _list;
+
+        public TPool[] List { get { return _list.ToArray(); } }
+
+        public ComponentsCollection(IEnumerable<TPool> initialCollection)
+        {
+            _list = new HashSet<TPool>(initialCollection);
+        }
+
+        public ComponentsCollection<TPool> Add(TPool component)
+        {
+            if(!_list.Add(component))
+                throw new ArgumentException($"Component {component} already in collection", nameof(component));
+
+            return this;
+        }
+
+        public TPool[] Build()
+        {
+            return _list.Where(i => i != null).ToArray();
+        }
+    }
+    public static class RentitasUtility
+    {
+        public static ComponentsCollection<TPool> CollectComponents<TPool>()
+            where TPool : IComponent
+        {
+            return CollectComponents<TPool>(typeof(TPool).Assembly);
+        }
+
+        public static ComponentsCollection<TPool> CollectComponents<TPool>(Assembly where)
+            where TPool : IComponent
+        {
+            return new ComponentsCollection<TPool>(where.GetTypes()
+                .Where(t => typeof(TPool).IsAssignableFrom(t) && !t.IsAbstract)
+                .Where(t => !t.GetCustomAttributes(true).Any(a => a is ObsoleteAttribute))
+                .Where(t => !t.IsGenericType)
+                .Select(t => (TPool) Activator.CreateInstance(t)));
+        }
+    }
+}
