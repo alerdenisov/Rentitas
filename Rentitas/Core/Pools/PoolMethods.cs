99e99292a87aa343d8845ae24e445f11cee85be2
Aler Denisov <aler.zampillo@gmail.com>
2017-01-19 13:15:04 +0300

    CreateComponent on Pool without entity instance

--- Rentitas/Core/Pools/PoolMethods.cs
+++ Rentitas/Core/Pools/PoolMethods.cs
@@ -13,5 +13,15 @@ namespace Rentitas
             for (int i = 0; i < _metaData.ComponentTypes.Length; i++)
                 ClearComponentPool(_metaData.ComponentTypes[i]);
         }
+
+        public T2 CreateComponent<T2>() where T2 : T, new()
+        {
+            var pool = ComponentPools[typeof (T2)];
+            if (pool.Count > 0)
+                return (T2)pool.Pop();
+
+            return new T2();
+        }
+
     }
 }
